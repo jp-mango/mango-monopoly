2d3f6a16f79fdc7380bb64691651c4346ffae022
mango <menziesjustin@ymail.com>
2024-07-15 13:25:56 -0400

    - added util to read csv

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -1,12 +1,15 @@
 package utils
 
 import (
+	"encoding/csv"
 	"fmt"
 	"io"
+	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
 // Function to download a file from a URL
@@ -39,14 +42,14 @@ func DownloadFile(filepath string, url string) error {
 	return nil
 }
 
-func RunPdfExtraction(script string) {
+func RunPdfExtraction(scriptPath string) {
 	fmt.Println("Extracting data to csv located in ./tax-auction/Gwinnett. Please wait...")
 
 	// create csv files
 	os.Create("../../tax-auction/Gwinnett/csv/Gwinnett-Past-Sales.csv")
 	os.Create("../../tax-auction/Gwinnett/csv/Gwinnett-Upcoming-Sales.csv")
 
-	cmd := exec.Command("cmd", "/C", ".venv\\Scripts\\activate && python", script)
+	cmd := exec.Command("cmd", "/C", ".venv\\Scripts\\activate && python", scriptPath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
@@ -55,4 +58,22 @@ func RunPdfExtraction(script string) {
 	}
 
 	fmt.Print("Data extraction complete\n\n")
+}
+
+func ReadCSV(filename string) [][]string {
+	dataPath := filepath.Join("tax-auction/Gwinnett/csv", filename)
+
+	file, err := os.Open(dataPath)
+	if err != nil {
+		log.Fatalf("Unable to open CSV: %v", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalf("Unable to parse CSV: %v", err)
+	}
+
+	return records
 }
