7d18ffca876d6d67a6f4c2ad06a1989cf7effbd7
mango <menziesjustin@ymail.com>
2024-07-16 22:56:00 -0400

    - updated schema

--- migrations/000001_create_tables.up.sql
+++ migrations/000001_create_tables.up.sql
@@ -4,10 +4,8 @@ CREATE TABLE IF NOT EXISTS Counties (county_id SERIAL PRIMARY KEY,
 
 
 CREATE TABLE IF NOT EXISTS Properties (property_id SERIAL PRIMARY KEY,
-																																								situs TEXT UNIQUE,
-																																								county_id INT, parcel_id VARCHAR(150) NOT NULL UNIQUE,
-																																								property_type TEXT, land_value NUMERIC, building_value NUMERIC, fair_market_value NUMERIC, lot_size NUMERIC, square_footage NUMERIC, bedrooms INT, bathrooms INT, year_built INT,
-																																							FOREIGN KEY (county_id) REFERENCES Counties(county_id));
+																																								situs TEXT, county_id INT, parcel_id VARCHAR(150) NOT NULL UNIQUE,
+																																								property_type TEXT, land_value NUMERIC, building_value NUMERIC, fair_market_value NUMERIC, lot_size NUMERIC, square_footage NUMERIC, bedrooms INT, bathrooms INT, year_built INT, tax_assessor_url VARCHAR(200));
 
 
 CREATE TABLE IF NOT EXISTS Property_Images (image_id SERIAL PRIMARY KEY,
