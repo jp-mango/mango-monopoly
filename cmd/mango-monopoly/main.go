7a35c6b8a747c8d55d7c22b2d5d4a3cce4523b89
mango <menziesjustin@ymail.com>
2024-07-18 13:01:29 -0400

    - tested paulding county data pull and translate

--- cmd/mango-monopoly/main.go
+++ cmd/mango-monopoly/main.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-	"fmt"
-	"log/slog"
-	"mango-monopoly/internal/db"
 	"mango-monopoly/internal/scraper"
-	"mango-monopoly/internal/utils"
 )
 
 func main() {
-	scraper.PullGwinnettAuctionData()
-	scraper.DownloadGwinnettAuctionData()
+	//scraper.PullGwinnettAuctionData()
+	//scraper.DownloadGwinnettAuctionData()
+	scraper.PullPauldingAuctionData()
+	scraper.DownloadPauldingAuctionData()
 
 	//db.ResetDB()
 
-	mango_monopoly, err := db.DbConnect()
-	if err != nil {
-		slog.Error("failed to connect to the database", "error", err)
-	}
-	defer mango_monopoly.Close()
+	//mango_monopoly, err := db.DbConnect()
+	//if err != nil {
+	//	slog.Error("failed to connect to the database", "error", err)
+	//}
+	//defer mango_monopoly.Close()
 
-	gwinnettPastSales := utils.ReadCSV("Gwinnett-Past-Sales.csv", "Gwinnett")
-	pastSalesRowsChanged, err := db.InsertGwinnettPastSalesData(gwinnettPastSales, mango_monopoly)
-	if err != nil {
-		slog.Error("failed to insert data into db", "error", err)
+	//gwinnettPastSales := utils.ReadCSV("Gwinnett-Past-Sales.csv", "Gwinnett")
+	//pastSalesRowsChanged, err := db.InsertGwinnettPastSalesData(gwinnettPastSales, mango_monopoly)
+	//if err != nil {
+	//	slog.Error("failed to insert data into db", "error", err)
 
-	}
-	fmt.Printf("Past sales rows changed: %d\n\n", pastSalesRowsChanged)
+	//}
+	//fmt.Printf("Past sales rows changed: %d\n\n", pastSalesRowsChanged)
 
-	gwinnettUpcomingSales := utils.ReadCSV("Gwinnett-Upcoming-Sales.csv", "Gwinnett")
-	upcomingSalesRowsChanged, err := db.InsertGwinnettUpcomingSalesData(gwinnettUpcomingSales, mango_monopoly)
-	if err != nil {
-		slog.Error("failed to insert data into db", "error", err)
-	}
-	fmt.Printf("Upcoming sales rows changed: %d\n\n", upcomingSalesRowsChanged)
+	//gwinnettUpcomingSales := utils.ReadCSV("Gwinnett-Upcoming-Sales.csv", "Gwinnett")
+	//upcomingSalesRowsChanged, err := db.InsertGwinnettUpcomingSalesData(gwinnettUpcomingSales, //mango_monopoly)
+	//if err != nil {
+	//	slog.Error("failed to insert data into db", "error", err)
+	//}
+	//fmt.Printf("Upcoming sales rows changed: %d\n\n", upcomingSalesRowsChanged)
 
-	fmt.Printf("New rows in Properties table: %d\n\n", pastSalesRowsChanged+upcomingSalesRowsChanged)
+	//fmt.Printf("New rows in Properties table: %d\n\n", pastSalesRowsChanged+upcomingSalesRowsChanged)
 
-	propertiesRowsChanged, err := db.UpdatePropertiesTable(mango_monopoly)
-	if err != nil {
-		slog.Error("failed to insert data into db", "error", err)
-
-	}
-	fmt.Printf("Rows changed in Properties table: %d\n\n", propertiesRowsChanged)
+	//startTime := time.Now()
+	//propertiesRowsChanged, err := db.UpdatePropertiesTable_Tax(mango_monopoly)
+	//if err != nil {
+	//	slog.Error("failed to insert data into db", "error", err)
+	//}
+	//elapsedTime := time.Since(startTime)
+	//fmt.Printf("Rows updated with tax assessor info in Properties table: %d\n\nProcess took: %v\n\n", propertiesRowsChanged, elapsedTime)
 }
