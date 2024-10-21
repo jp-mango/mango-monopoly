b549c574dffd2d7672172ab0ecdeda9e3ca8b9d8
mango <menziesjustin@ymail.com>
2024-10-21 09:41:50 -0400

    -

--- migrations/000002_create_triggers.down.sql
+++
@@ -1,8 +0,0 @@
-DROP TRIGGER IF EXISTS insert_property_on_upcoming_sales_insert ON Upcoming_Sales;
-
-
-DROP TRIGGER IF EXISTS insert_property_on_past_sales_insert ON Past_Sales;
-
-
-DROP FUNCTION IF EXISTS insert_property_if_not_exists();
-
