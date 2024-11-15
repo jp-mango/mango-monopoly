e93664df6003ab4b7a2f3ac6cbb9de00cbdf0c69
mango <menziesjustin@ymail.com>
2024-11-15 12:12:25 -0500

    - added sql migrations

---
+++ migrations/000001_create_tables.down.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS Property_Images;
+
+
+DROP TABLE IF EXISTS Upcoming_Sales;
+
+
+DROP TABLE IF EXISTS Past_Sales;
+
+
+DROP TABLE IF EXISTS Properties;
+
+
+DROP TABLE IF EXISTS Counties;
+
+
+DROP TABLE IF EXISTS "Sessions";
+
