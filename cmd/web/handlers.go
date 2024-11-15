4e766f391d51fbf76ebfec4133d20496cb9b6c34
mango <menziesjustin@ymail.com>
2024-11-15 12:13:22 -0500

    - cleaned up create property handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"errors"
-	"fmt"
 	"mango-monopoly/internal/models"
 	"net/http"
 	"strconv"
@@ -15,6 +13,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) viewAllProperties(w http.ResponseWriter, r *http.Request) {
+
 	app.render(w, r, http.StatusOK, "properties.tmpl", templateData{})
 }
 
@@ -41,27 +40,6 @@ func (app *application) propertyView(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-// TODO: accept user input
 func (app *application) createProperty(w http.ResponseWriter, r *http.Request) {
-	//dummy data
-	property := &models.Property{
-		Address:         sql.NullString{String: "45 Wallaby Way."},
-		City:            sql.NullString{String: "Sydney"},
-		State:           sql.NullString{String: "Australia"},
-		Zip:             sql.NullString{String: "1337"},
-		ParcelID:        sql.NullString{String: "DU"},
-		PropertyType:    sql.NullString{String: "land"},
-		LandValue:       sql.NullInt64{Int64: 0},
-		BuildingValue:   sql.NullInt64{Int64: 24500000},
-		FairMarketValue: sql.NullInt64{Int64: 27650000},
-		LotSize:         sql.NullFloat64{Float64: 102.8},
-	}
-
-	id, err := app.properties.Insert(property)
-	if err != nil {
-		app.serverError(w, r, err)
-		return
-	}
-
-	http.Redirect(w, r, fmt.Sprintf("/property/%d", id), http.StatusSeeOther)
+	app.render(w, r, http.StatusOK, "createProperty.tmpl", templateData{})
 }
