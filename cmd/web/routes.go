6d4cd7020e8abb6ab1e92b036e933727051ce660
mango <menziesjustin@ymail.com>
2024-11-18 18:26:54 -0500

    - created routes for user onboarding

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,11 +19,15 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home)) //the {$} prevents wildcard matching
 
 	mux.Handle("GET /properties", dynamic.ThenFunc(app.viewAllProperties))
-
 	mux.Handle("GET /property/{id}", dynamic.ThenFunc(app.propertyView))
-
 	mux.Handle("GET /property/create", dynamic.ThenFunc(app.createPropertyPage))
 	mux.Handle("POST /property/create", dynamic.ThenFunc(app.propertyCreatePost))
+
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
 	standard := chain.New(app.recoverPanic, app.logRequest, commonHeaders)
 
