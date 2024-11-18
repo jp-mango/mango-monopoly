1d2373ae4c6e246c4de51a39c56d19d7eda98968
mango <menziesjustin@ymail.com>
2024-11-17 21:34:59 -0500

    - fixed form handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -53,7 +53,8 @@ func (app *application) propertyView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) createPropertyPage(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, http.StatusOK, "createProperty.tmpl", templateData{})
+	data := app.newTemplateData(r)
+	app.render(w, r, http.StatusOK, "createProperty.tmpl", data)
 }
 
 func (app *application) propertyCreatePost(w http.ResponseWriter, r *http.Request) {
@@ -65,84 +66,84 @@ func (app *application) propertyCreatePost(w http.ResponseWriter, r *http.Reques
 
 	prop := models.Property{}
 
-	prop.Address = sql.NullString{String: r.PostForm.Get("situs")}
+	prop.Address = sql.NullString{String: r.PostForm.Get("situs"), Valid: true}
 
-	prop.City = sql.NullString{String: r.PostForm.Get("city")}
+	prop.City = sql.NullString{String: r.PostForm.Get("city"), Valid: true}
 
-	prop.State = sql.NullString{String: r.PostForm.Get("state")}
+	prop.State = sql.NullString{String: r.PostForm.Get("state"), Valid: true}
 
-	prop.Zip = sql.NullString{String: r.PostForm.Get("zip_code")}
+	prop.Zip = sql.NullString{String: r.PostForm.Get("zip_code"), Valid: true}
 
-	prop.County = sql.NullString{String: r.PostForm.Get("county_id")}
+	prop.County = sql.NullString{String: r.PostForm.Get("county_id"), Valid: true}
 
-	prop.ParcelID = sql.NullString{String: r.PostForm.Get("parcel_id")}
+	prop.ParcelID = sql.NullString{String: r.PostForm.Get("parcel_id"), Valid: true}
 
-	prop.PropertyType = sql.NullString{String: r.PostForm.Get("property_type")}
+	prop.PropertyType = sql.NullString{String: r.PostForm.Get("property_type"), Valid: true}
 
 	landValue, err := strconv.ParseInt(r.PostForm.Get("land_value"), 10, 64)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.LandValue = sql.NullInt64{Int64: landValue}
+	prop.LandValue = sql.NullInt64{Int64: landValue, Valid: true}
 
 	buildingValue, err := strconv.ParseInt(r.PostForm.Get("building_value"), 10, 64)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.BuildingValue = sql.NullInt64{Int64: buildingValue}
+	prop.BuildingValue = sql.NullInt64{Int64: buildingValue, Valid: true}
 
 	fairMarketValue, err := strconv.ParseInt(r.PostForm.Get("fair_market_value"), 10, 64)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.FairMarketValue = sql.NullInt64{Int64: fairMarketValue}
+	prop.FairMarketValue = sql.NullInt64{Int64: fairMarketValue, Valid: true}
 
 	lotSize, err := strconv.ParseFloat(r.PostForm.Get("lot_size"), 64)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.LotSize = sql.NullFloat64{Float64: lotSize}
+	prop.LotSize = sql.NullFloat64{Float64: lotSize, Valid: true}
 
 	squareFootage, err := strconv.ParseInt(r.PostForm.Get("square_footage"), 10, 64)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.SquareFt = sql.NullInt64{Int64: squareFootage}
+	prop.SquareFt = sql.NullInt64{Int64: squareFootage, Valid: true}
 
 	bedrooms, err := strconv.ParseInt(r.PostForm.Get("bedrooms"), 10, 16)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.Bedrooms = sql.NullInt16{Int16: int16(bedrooms)}
+	prop.Bedrooms = sql.NullInt16{Int16: int16(bedrooms), Valid: true}
 
 	bathrooms, err := strconv.ParseFloat(r.PostForm.Get("bathrooms"), 64)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.Bathrooms = sql.NullFloat64{Float64: bathrooms}
+	prop.Bathrooms = sql.NullFloat64{Float64: bathrooms, Valid: true}
 
 	yearBuilt, err := strconv.ParseInt(r.PostForm.Get("year_built"), 10, 16)
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	prop.YearBuilt = sql.NullInt16{Int16: int16(yearBuilt)}
+	prop.YearBuilt = sql.NullInt16{Int16: int16(yearBuilt), Valid: true}
 
-	prop.TaxURL = sql.NullString{String: r.PostForm.Get("tax_assessor_url")}
-	prop.ZillowURL = sql.NullString{String: r.PostForm.Get("zillow_url")}
+	prop.TaxURL = sql.NullString{String: r.PostForm.Get("tax_assessor_url"), Valid: true}
+	prop.ZillowURL = sql.NullString{String: r.PostForm.Get("zillow_url"), Valid: true}
 
-	id, err := app.properties.Insert(&prop)
+	prop.ID, err = app.properties.Insert(&prop)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/property/view/%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/property/view/%d", prop.ID), http.StatusSeeOther)
 }
