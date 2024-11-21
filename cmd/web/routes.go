92f24de965d5210f936b88b478a03424547d6b4a
mango <menziesjustin@ymail.com>
2024-11-21 15:14:16 -0500

    - added and implemented middleware for protected routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,17 +16,18 @@ func (app *application) routes() http.Handler {
 	dynamic := chain.New(app.sessionManager.LoadAndSave)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home)) //the {$} prevents wildcard matching
-
-	mux.Handle("GET /properties", dynamic.ThenFunc(app.viewAllProperties))
 	mux.Handle("GET /property/{id}", dynamic.ThenFunc(app.propertyView))
-	mux.Handle("GET /property/create", dynamic.ThenFunc(app.createPropertyPage))
-	mux.Handle("POST /property/create", dynamic.ThenFunc(app.propertyCreatePost))
-
+	mux.Handle("GET /properties", dynamic.ThenFunc(app.viewAllProperties))
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
 	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
 	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
 	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
-	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
+	protected := dynamic.Append(app.requireAuthentication)
+
+	mux.Handle("GET /property/create", protected.ThenFunc(app.createPropertyPage))
+	mux.Handle("POST /property/create", protected.ThenFunc(app.propertyCreatePost))
+	mux.Handle("POST /user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	standard := chain.New(app.recoverPanic, app.logRequest, commonHeaders)
 
